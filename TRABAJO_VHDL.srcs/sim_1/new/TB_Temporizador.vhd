f82a6073ec5052c45fb57047e6ed330413c2c2f1
ArielCamposM <ariel.campos@alumnos.upm.es>
2025-12-16 11:43:24 +0100

    Se crea modulo de temporizador
    Este modulo contara cada ms en funcion del reloj de la fpga. Se ha hecho un banco de pruebas para comprobas que cuenta como es debido

---
+++ TRABAJO_VHDL.srcs/sim_1/new/TB_Temporizador.vhd
@@ -0,0 +1,36 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity TB_Temporizador is
+end entity;
+
+architecture Behavioral of TB_Temporizador is
+
+    constant CLK_PERIOD : time := 10 ns; -- 100 MHz
+
+    signal clk      : std_logic := '0';
+    signal reset    : std_logic := '1';
+    signal tick_1ms : std_logic;
+
+begin
+
+    clk <= not clk after CLK_PERIOD/2;
+  
+    DUT : entity work.Temporizador
+        port map(
+            clk      => clk,
+            reset    => reset,
+            tick_1ms => tick_1ms
+        );
+
+    stim_proc : process
+    begin
+        wait for 50 ns;
+        reset <= '0';
+        wait for 5 ms; --veremos cinco pulsos del temporizador
+
+        assert false report "Fin de simulación" severity failure;
+    end process;
+
+end architecture;
---
+++ TRABAJO_VHDL.srcs/sources_1/new/Temporizador.vhd
@@ -0,0 +1,35 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity Temporizador is
+    port(
+        clk      : in  std_logic;      -- 100 MHz
+        reset    : in  std_logic;
+        tick_1ms : out std_logic --pulso 1 ciclo/ms
+    );
+end entity;
+
+architecture Behavioral of Temporizador is
+    constant MAX_COUNT : unsigned(16 downto 0) := to_unsigned(100000 - 1, 17); --valor maximo contadores
+    signal count : unsigned(16 downto 0) := (others => '0');--registro contador interno. 17 bits
+begin
+
+    process(clk, reset)
+    begin
+        if reset = '1' then
+            count    <= (others => '0');
+            tick_1ms <= '0';
+
+        elsif rising_edge(clk) then
+            if count = MAX_COUNT then
+                count    <= (others => '0');
+                tick_1ms <= '1'; --cuenta 1 cada ms (vale 1 un ciclo de reloj cada 100k ciclos)
+            else
+                count    <= count + 1;
+                tick_1ms <= '0';
+            end if;
+        end if;
+    end process;
+
+end Behavioral;
